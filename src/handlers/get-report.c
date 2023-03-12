f353ad0a142cb079e639c8db69a37b8f1d2f36ca
Daviiap <davii.sapiens@gmail.com>
2023-03-12 18:19:12 -0300

    feat: reading report mock from file

--- src/handlers/get-report.c
+++ src/handlers/get-report.c
@@ -3,6 +3,26 @@
 #include "../snp/psp-sev.h"
 #include "../snp/attestation.h"
 
+int read_file_bytes(const char *filename, char *buffer, int length)
+{
+    struct file *fp;
+    loff_t pos = 0;
+    int bytes_read = 0;
+
+    // open the file in read mode
+    fp = filp_open(filename, O_RDONLY, 0);
+    if (fp == NULL)
+        return -ENOENT; // file not found error
+
+    // read bytes from the file
+    bytes_read = kernel_read(fp, buffer, length, &pos);
+
+    // close the file
+    filp_close(fp, NULL);
+
+    return bytes_read;
+}
+
 static int handle_get_report(unsigned long guest_request_ptr)
 {
     printk(KERN_INFO "sev-guest: received get attestation report");
@@ -29,8 +49,17 @@ static int handle_get_report(unsigned long guest_request_ptr)
     }
 
     report_resp->status = 0;
-    report_resp->report_size = 1184;
-    report_resp->report.policy = 10;
+    report_resp->report_size = sizeof(report_resp->report);
+
+    char buffer[1184];
+    int bytes_read = read_file_bytes("/report.bin", buffer, 1184);
+    if (bytes_read < 0)
+    {
+        printk(KERN_ERR "Error reading file\n");
+        return bytes_read;
+    }
+
+    memcpy(&report_resp->report, &buffer, sizeof(buffer));
 
     if (copy_to_user((int *)guest_request_ptr, &ioctl_request, sizeof(ioctl_request)))
     {
